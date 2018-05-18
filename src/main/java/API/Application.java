33658eeafdb2d34e82f5dc894ad5e0fd46115b0d
beervoley <sevkorobot@gmail.com>
2018-05-18 11:42:14 -0700

    Basic Spring Boot API

---
+++ src/main/java/API/Application.java
@@ -0,0 +1,18 @@
+/**
+ * Created by vsevolod on 2018-05-17.
+ */
+
+package API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
