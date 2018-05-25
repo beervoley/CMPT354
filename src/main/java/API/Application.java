fbd278f83eec35df058fb66db501ac6025658001
beervoley <sevkorobot@gmail.com>
2018-05-24 17:13:04 -0700

    Add the JDBC Driver library + test connection to DB

--- src/main/java/API/Application.java
+++ src/main/java/API/Application.java
@@ -7,12 +7,23 @@ package API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-@SpringBootApplication
+
+//@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection connection = DriverManager.getConnection("jdbc:sqlserver://cypress.csil.sfu.ca;" +
+                "user=s_viliushi;" +
+                "password=7Lta2646mdrAGhrP");
+        System.out.println(connection.getCatalog());
+
+      //  SpringApplication.run(Application.class, args);
     }
 
 }
