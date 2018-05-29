65ae4b15655b4b9d54bf60f9845f0b30a7a37509
beervoley <sevkorobot@gmail.com>
2018-05-28 18:10:33 -0700

    Upload testcode for basic queries

--- src/main/java/API/Application.java
+++ src/main/java/API/Application.java
@@ -7,12 +7,10 @@ package API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -21,9 +19,19 @@ public class Application {
         Connection connection = DriverManager.getConnection("jdbc:sqlserver://cypress.csil.sfu.ca;" +
                 "user=s_viliushi;" +
                 "password=7Lta2646mdrAGhrP");
-        System.out.println(connection.getCatalog());
-
-      //  SpringApplication.run(Application.class, args);
+//        System.out.println(connection.getCatalog());
+//        Statement stl = null;
+//        String queryToCreate = "CREATE TABLE Student" +
+//                "(sid INTEGER," +
+//                "name CHAR(20)," +
+//                "address CHAR(30)," +
+//                "phone CHAR(13)," +
+//                "major CHAR(4))";
+//        String queryToDelete = "DROP TABLE Student";
+//        stl = connection.createStatement();
+//        stl.executeQuery(query);
+        connection.close();
+        SpringApplication.run(Application.class, args);
     }
 
 }
